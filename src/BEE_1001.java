7562498bc93017fb70caf8e5d761d8301810140a
Filipe <filipe.cutri18@gmail.com>
2024-08-28 20:46:05 -0300

    BEE-1001

---
+++ src/BEE_1001.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class BEE_1001 {
+    public static void main(String[] args) {
+        int X;
+
+        Scanner read = new Scanner(System.in);
+        System.out.print("Digite um número inteiro: ");
+        int A = read.nextInt();
+
+        System.out.print("Digite um número inteiro: ");
+        int B = read.nextInt();
+
+        X = A + B;
+        System.out.println("X = " + X);
+    }
+}
+
