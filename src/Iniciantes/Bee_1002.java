6c8935364b6cc66087637d3c29bf0cd5950925d0
Filipe <filipe.cutri18@gmail.com>
2024-10-03 23:31:19 -0300

    BEE-1002

--- src/Iniciantes/Bee_1002.java
+++ src/Iniciantes/Bee_1002.java
@@ -1,19 +1,14 @@
 package Iniciantes;
-import java.util.Locale;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Bee_1002 {
-    public static void main(String[] args)  {
-        Locale.setDefault(Locale.US);
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
 
-        double area;
-        double raio;
-        double n = 3.14159;
+        double raio = leitor.nextDouble();
+        double area = 3.14159 * (raio * raio);
 
-        Scanner scanner = new Scanner(System.in);
-        raio = scanner.nextDouble();
-
-        area = n * (raio*2);
-        System.out.printf("A=%.4f%n", area);
+        System.out.println(String.format("A=%.4f", area));
     }
 }
